85fcb18efd471ab1290b921204ddf128a1ecd557
Aurelio <aureliostrazzeri@gmail.com>
2026-07-08 18:29:20 +0200

    get_start_loop fix?

--- action_provider/action_provider_dreamzero.py
+++ action_provider/action_provider_dreamzero.py
@@ -228,6 +228,9 @@ class DreamZeroActionProvider(ActionProvider):
 
         return obs
 
+    def get_start_loop(self):
+        return False
+
     def get_action(self, env) -> Optional[torch.Tensor]:
         try:
             if self._action_queue:
